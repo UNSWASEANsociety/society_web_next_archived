d7e0f287ccdd8086e5d0c24c87231d956462117d
Tymotex <timzhang3@gmail.com>
2021-03-13 17:46:43 +1100

    Added vision/mission section

---
+++ components/Youtube/EmbeddedPlayer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import YouTube from 'react-youtube';
+
+const EmbeddedPlayer = ({ videoId }) => {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+      },
+    };
+    const onReady = (event) => {
+        // access to player in all event handlers via event.target
+        event.target.pauseVideo();
+    };
+    
+    return <YouTube videoId={videoId} opts={opts} onReady={onReady} />;
+
+}
+
+export default EmbeddedPlayer;
